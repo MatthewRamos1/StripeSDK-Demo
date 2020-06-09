9708e3bfe975d14bbd1a3776451dea4d5f94ebda
Matthew Ramos <mmramos309@gmail.com>
2020-06-09 00:01:02 -0400

    Installed stripe terminal pods

---
+++ StripeSDK-Demo/Pods/Target Support Files/Pods-StripeSDK-Demo/Pods-StripeSDK-Demo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StripeSDK_Demo : NSObject
+@end
+@implementation PodsDummy_Pods_StripeSDK_Demo
+@end
---
+++ StripeSDK-Demo/Pods/Target Support Files/Pods-StripeSDK-DemoTests/Pods-StripeSDK-DemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StripeSDK_DemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_StripeSDK_DemoTests
+@end
